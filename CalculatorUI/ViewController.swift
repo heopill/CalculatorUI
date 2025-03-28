0eaf0c12474474aa9dc3a5179cdf2144df7fc62c
heopill <heopill@naver.com>
2025-03-28 16:38:37 +0900

    feat: make Lv2 code Lv2 기능 구현

--- CalculatorUI/ViewController.swift
+++ CalculatorUI/ViewController.swift
@@ -9,12 +9,33 @@ import UIKit
 import SnapKit
 
 class ViewController: UIViewController {
-    let label = UILabel()
+    let label = UILabel() // label 생성
+    let horizontalstackView = UIStackView() // stackView 생성
+    
+    private func makeButton(_ text: String) -> UIButton {
+        let button = UIButton()
+        
+        button.setTitle(text, for: .normal)
+        button.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 30)
+        
+        horizontalstackView.addArrangedSubview(button)
+        
+        button.snp.makeConstraints{ make in
+            make.width.height.equalTo(80)
+        }
+        
+        return button
+    }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
+        makeButton("7")
+        makeButton("8")
+        makeButton("9")
+        makeButton("+")
     }
     
     private func configureUI() {
@@ -24,10 +45,16 @@ class ViewController: UIViewController {
         label.textAlignment = .right
         label.font = .boldSystemFont(ofSize: 60)
         
-//        [label]
-//            .forEach { view.addSubview($0) }
+        horizontalstackView.axis = .horizontal
+        horizontalstackView.backgroundColor = .black
+        horizontalstackView.spacing = 10
+        horizontalstackView.distribution = .fillEqually
         
-        view.addSubview(label)
+        
+        [label, horizontalstackView]
+            .forEach { view.addSubview($0) }
+        
+        // view.addSubview(label)
         
         
         label.snp.makeConstraints{ make in
@@ -37,6 +64,13 @@ class ViewController: UIViewController {
             make.height.equalTo(100)
         }
         
+        horizontalstackView.snp.makeConstraints{ make in
+            make.height.equalTo(80)
+            make.top.equalTo(label.snp.bottom).offset(60)
+            make.centerX.equalToSuperview()
+        }
     }
+    
+    
 }
 
